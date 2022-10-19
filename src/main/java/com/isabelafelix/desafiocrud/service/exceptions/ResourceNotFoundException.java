75ecf7d2d4836b224b350577083e9c9dd105748d
Isabela Felix <isabbelafelix@gmail.com>
2022-10-19 19:55:31 -0300

    change code

--- src/main/java/com/isabelafelix/desafiocrud/service/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/isabelafelix/desafiocrud/service/exceptions/ResourceNotFoundException.java
@@ -1,6 +1,7 @@
 package com.isabelafelix.desafiocrud.service.exceptions;
 
 public class ResourceNotFoundException extends RuntimeException{
+    private static final long serialVersionUID = 1L;
 
     public ResourceNotFoundException(String msg){
         super(msg);
